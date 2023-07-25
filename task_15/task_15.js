ba07f159d5d389662f82eab02b6e847a572783fc
Volha Klemiatsenka <vklemiatsenka@gmail.com>
2023-07-26 00:58:58 +0200

    Completed task_15.js

---
+++ task_15/task_15.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/*  Ваша задача - отсортировать заданную строку. Каждое слово в строке будет содержать одно число. Это число - позиция, которую слово должно занимать в результате.
+ Примечание: числа могут быть от 1 до 9. Таким образом, 1 будет первым словом (не 0).
+ Если входная строка пуста, вернуть пустую строку. Слова во входной строке будут содержать только последовательные числа.
+ Например: is2 Thi1s T4est 3a -> Thi1s is2 3a T4est
+*/
+
+function sortedStr(str) {
+    if (str.length === 0) {
+        return '';
+    } else {
+        const arr = str.split(' ');
+        let result = [];
+        arr.forEach(item => {
+            for (let i = 0; i <= item.length - 1; i++) {
+                if (isFinite(item[i])) {
+                    result[item[i] - 1] = item;
+                }
+            }
+        });
+        return result.join(' ');
+    }
+}
+
+console.log(sortedStr('is2 Thi1s T4est 3a'));
